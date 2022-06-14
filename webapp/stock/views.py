762f01a06b2ee5e379eedbc67d4e4d95adcf60c3
won yong Chung <58002931+wonyongchung@users.noreply.github.com>
2022-06-14 17:45:42 +0900

    commit

--- webapp/stock/views.py
+++ webapp/stock/views.py
@@ -41,9 +41,21 @@ def stock_detail_dj(request):
         word = request.POST.get('stockname').split('-')[-1]
         print(word)
         predicted_stock_price = 0
-
+        quantity = request.POST.get('quantity')
+        print(quantity)
         df = pd.read_csv('webapp/media/{}/{}.csv'.format(word, word), encoding='cp949')
-
+        currentprice = df["종가"][len(df)-1]
+        predictprice = pd.read_csv('webapp/media/predict.csv')
+        print(predictprice)
+        predictprice = predictprice[word]
+        print(predictprice)
+        predictprice = predictprice.iloc[int(quantity)]
+        print(predictprice)
+        surgerate = round(100*((predictprice-currentprice)/currentprice),2)
+        if surgerate>0:
+            triangle = 1
+        else:
+            triangle = None
         xy = df.iloc[:,1:5].values
         window, past, predict_day, future_price = 7, 8, 1, 5       
         trainSet, testSet = MinMaxScaler(xy[0:-past]), MinMaxScaler(xy[-past-window:])  # past 가장 최근의 과거 일주일만 보여줌
@@ -71,20 +83,20 @@ def stock_detail_dj(request):
         model.fit(X_train,y_train)
 
         predicted_stock_price = model.predict(X_test)[:,-1]  # 예측할 때는 맨 마지막 종가 예측 결과만 출력해준다
-        print(df.iloc[-past:,-1])
-        print("가자아아아ㅏ앙", df.iloc[-past:,-1])
+        #print(df.iloc[-past:,-1])
+        #print("가자아아아ㅏ앙", df.iloc[-past:,-1])
         predicted_stock_price = back_MinMax(df.iloc[-past:,-1], predicted_stock_price)
         
-        print("예측된 결과는 : ", predicted_stock_price.shape)
-        print("원본 테스트 크기 :", tmp_y_test.shape)
-        print("이;겆",xy.shape, model.predict(X_test)[-1].reshape(1,4).shape)
+        #print("예측된 결과는 : ", predicted_stock_price.shape)
+        #print("원본 테스트 크기 :", tmp_y_test.shape)
+        #print("이;겆",xy.shape, model.predict(X_test)[-1].reshape(1,4).shape)
         xy = np.append(xy, model.predict(X_test)[-1].reshape(1,4), axis=0)   # 예측된 맨 마지막 결과를 넣어서 이제부터 미래 예측
         for i in range(future_price):
             trainSet, testSet = MinMaxScaler(xy[0:-past]), MinMaxScaler(xy[-past-window:])  # past 가장 최근의 과거 일주일만 가지고 새로 학습
 
             X_train, y_train = buildDataSet(trainSet, window)
             X_test, y_test = buildDataSet(testSet, window)
-            print("테스트는?",X_test.shape )
+            #print("테스트는?",X_test.shape )
             # RF는 2차원 데이터를 입력으로 받아서 1차원으로 바꿔줘야한다.
             X_train = X_train.reshape(X_train.shape[0],7*4)
             X_test = X_test.reshape(X_test.shape[0],7*4)
@@ -93,10 +105,10 @@ def stock_detail_dj(request):
             #model.fit(X_train, y_train)
 
             future_predicted_stock_price = model.predict(X_test)
-            print("이거", df.iloc[-past:,])
+            #print("이거", df.iloc[-past:,])
             future_predicted_stock_price = back_MinMax(df.iloc[-past:,-1], future_predicted_stock_price)  # 여기서는 전체로 되돌려줌
 
-            print("응", future_predicted_stock_price.shape)
+            #print("응", future_predicted_stock_price.shape)
             xy = np.append(xy, future_predicted_stock_price[-1].reshape(1,4), axis=0)   # 예측된 맨 마지막 결과를 넣어서 이제부터 미래 예측
 
         org_df1 = pd.DataFrame(back_MinMax(df.iloc[-5:,-1], tmp_y_test)).reset_index(drop=True)
@@ -108,8 +120,8 @@ def stock_detail_dj(request):
         df4 = pd.concat([df3, df1, df2], axis=1)
         # df4 = df4.fillna(0)
         df = df4.values.tolist()
-        print("df가 어찌되는겨", df4)
-        print("더해진 5개가 있나요?", xy.shape)
+        #print("df가 어찌되는겨", df4)
+        #print("더해진 5개가 있나요?", xy.shape)
         ratio = -1
         it = 0
         dt = 0
@@ -125,4 +137,4 @@ def stock_detail_dj(request):
         showstock = pd.read_csv(os.path.join(os.getcwd(), "webapp", "media", "상장법인목록.csv"), encoding='cp949', index_col=1)
         showstock.index = [format(code, '06') for code in showstock.index]
         showstock = showstock.loc[word]
-        return render(request, 'front/stock_detail_dj.html', {'showstock': showstock, 'df':df, 'ratio':ratio, 'it':it, 'dt':dt})
+        return render(request, 'front/stock_detail_dj.html', {'showstock': showstock, 'df':df, 'ratio':ratio, 'it':it, 'dt':dt, 'surgerate':surgerate, 'currentprice':currentprice, 'triangle':triangle})
