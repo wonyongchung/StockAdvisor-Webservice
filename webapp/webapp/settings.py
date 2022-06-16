983ec8c43eaa773681df2bf9ac9645b455a3d799
djkim0516 <73953214+djkim0516@users.noreply.github.com>
2022-06-16 15:41:54 +0900

    Allowed host update

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'e7s+*fg@po76%s@0hk!5l1$8v6)dpxxa=y5^e0vfl2($0ys#4u'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '.stockpredict.co.kr',
+]
 
 
 # Application definition
