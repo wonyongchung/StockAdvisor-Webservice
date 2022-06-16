729fd74181a5839ac4c569842e28abd01336c619
17011813 <48427281+17011813@users.noreply.github.com>
2022-06-16 22:42:30 +0900

    predict.csv 예측 파일 만듦

--- webapp/stock/data_update.py
+++ webapp/stock/data_update.py
@@ -4,20 +4,32 @@ from pytz import timezone
 from pykrx import stock
 import glob, os
 from celery import shared_task
-
 import numpy as np
-from sklearn.preprocessing import MinMaxScaler
+#from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
-from torch.utils.data import DataLoader, Dataset
-from torch.nn import Transformer
-from torch import nn
-import torch
 import math
 from keras.models import Sequential
 from keras.layers import Dense, LSTM, Conv1D, Lambda
 from keras.losses import Huber
 from keras.optimizers import Adam
 from keras.callbacks import EarlyStopping, ModelCheckpoint
+from django.shortcuts import render
+import numpy as np
+import pandas as pd
+import os, glob
+from sklearn.ensemble import RandomForestRegressor
+from pykrx import stock as pystock
+
+def MinMaxScaler(data):
+    denom = np.max(data,0)-np.min(data,0)            # np.min(data,0) 이었는데 - 값이 있어서 0이 더 작은 값으로 되서 0으로 나누는 경우 에러
+    nume = data-np.min(data,0)
+    return nume/denom
+
+# 정규화 되돌리기 함수 
+def back_MinMax(data,value):
+    diff = np.max(data,0)-np.min(data,0)
+    back = value * diff + np.min(data,0)
+    return back 
 
 @shared_task
 def update_data():
@@ -62,166 +74,57 @@ def update_data():
         data = stock.get_market_ohlcv_by_date(fromdate=time_start_str, todate=time_now_str, ticker=ticker)
         data.to_csv(f"{ticker_dir}/{ticker}.csv", encoding='cp949')
 
+    total = []
 
     for media in market_cap:
         media_dir = os.path.join(data_dir , f"{media}")
-        data = pd.read_csv(f"{media_dir}/{media}.csv", encoding='cp949')
-        
-        scaler = MinMaxScaler()
-        data['종가'] = scaler.fit_transform(data['종가'].to_numpy().reshape(-1, 1))
+        df = pd.read_csv(f"{media_dir}/{media}.csv", encoding='cp949')
 
-        train = data[:-30]
-        train_data = train['종가'].to_numpy()
+        xy = df.iloc[:,-2].values   # 종가만 가져다가 예측
 
-        test = data[-30:]
-        test_data = test['종가'].to_numpy()
+        window, future_price  = 90, 30  
+        train_size = int(len(xy)) - window
+        trainSet = MinMaxScaler(xy) # past 가장 최근의 과거 일주일만 보여줌
 
-        class windowDataset(Dataset):
-            def __init__(self, y, input_window=80, output_window=20, stride=5):
-                #총 데이터의 개수
-                L = y.shape[0]
-                #stride씩 움직일 때 생기는 총 sample의 개수
-                num_samples = (L - input_window - output_window) // stride + 1
+        def buildDataSet(data, window):
+            xdata, ydata = [], []
+            for i in range(0, len(data) - window):
+                xdata.append(data[i:i + window])                           # 행은 10개씩, 열은 직전의 Number 값들도 같이 입력변수로 넣어준다
+                ydata.append(data[i + window])                   # 행은 그 다음 행 하나랑, 열은 모든 feature
+            return np.array(xdata), np.array(ydata)
 
-                #input과 output
-                X = np.zeros([input_window, num_samples])
-                Y = np.zeros([output_window, num_samples])
+        X_train, y_train=buildDataSet(trainSet, window)
 
-                for i in np.arange(num_samples):
-                    start_x = stride*i
-                    end_x = start_x + input_window
-                    X[:,i] = y[start_x:end_x]
+        # RF는 2차원 데이터를 입력으로 받아서 1차원으로 바꿔줘야한다.
+        X_train = X_train.reshape(X_train.shape[0],window)
 
-                    start_y = stride*i + input_window
-                    end_y = start_y + output_window
-                    Y[:,i] = y[start_y:end_y]
+        print("맨 처음 훈련 셋 좀 보자", X_train.shape,  y_train.shape)
+        model = RandomForestRegressor()
+        model.fit(X_train,y_train)   # 예측할 때는 맨 마지막 종가 예측 결과만 출력해준다
 
-                X = X.reshape(X.shape[0], X.shape[1], 1).transpose((1,0,2))
-                Y = Y.reshape(Y.shape[0], Y.shape[1], 1).transpose((1,0,2))
-                self.x = X
-                self.y = Y
-                
-                self.len = len(X)
-            def __getitem__(self, i):
-                return self.x[i], self.y[i]
-            def __len__(self):
-                return self.len
+        predicted_stock_price = model.predict(X_train[-1:])  # 예측할 때는 맨 마지막 종가 예측 결과만 출력해준다
+        predicted_stock_price = back_MinMax(df.iloc[train_size-window:,-2], predicted_stock_price)
+        print("맨 처음에 예측한 미래의 결과는?", predicted_stock_price.shape)
+        xy = np.append(xy, predicted_stock_price)   # 예측된 맨 마지막 결과를 넣어서 이제부터 미래 예측
+        for p in range(future_price):
+            print("현재 총 데이터셋은 ", xy.shape)   # 원래 1228개 있다가 1229개로 늘어남
+            trainSet = MinMaxScaler(xy)   # past 가장 최근의 과거 window 사이즈만 가지고 새로 학습
+            X_train, y_train = buildDataSet(trainSet, window)
 
-        iw = 60 # 14
-        ow = 30 # 7
+            # RF는 2차원 데이터를 입력으로 받아서 1차원으로 바꿔줘야한다.
+            X_train = X_train.reshape(X_train.shape[0],window)
+            y_train = y_train.reshape(y_train.shape[0],)
 
-        train_dataset = windowDataset(train_data, input_window=iw, output_window=ow, stride=1)
-        train_loader = DataLoader(train_dataset, batch_size=64)
+            future_predicted_stock_price = model.predict(X_train[-1:])
+            future_predicted_stock_price = back_MinMax(df.iloc[train_size-window:,-2], future_predicted_stock_price)  # 여기서는 전체로 되돌려줌
+            print("추가할 정보", future_predicted_stock_price.shape)
+            xy = np.append(xy, future_predicted_stock_price, axis=0)   # 예측된 맨 마지막 결과를 넣어서 이제부터 미래 예측
 
+        total.append(xy[-future_price:])
 
-        class TFModel(nn.Module):
-            def __init__(self,iw, ow, d_model, nhead, nlayers, dropout=0.5):
-                super(TFModel, self).__init__()
-                self.encoder_layer = nn.TransformerEncoderLayer(d_model=d_model, nhead=nhead, dropout=dropout)
-                self.transformer_encoder = nn.TransformerEncoder(self.encoder_layer, num_layers=nlayers) 
-                self.pos_encoder = PositionalEncoding(d_model, dropout)
-
-                self.encoder = nn.Sequential(
-                    nn.Linear(1, d_model//2),
-                    nn.ReLU(),
-                    nn.Linear(d_model//2, d_model)
-                )
-                
-                self.linear =  nn.Sequential(
-                    nn.Linear(d_model, d_model//2),
-                    nn.ReLU(),
-                    nn.Linear(d_model//2, 1)
-                )
-
-                self.linear2 = nn.Sequential(
-                    nn.Linear(iw, (iw+ow)//2),
-                    nn.ReLU(),
-                    nn.Linear((iw+ow)//2, ow)
-                ) 
-
-            def generate_square_subsequent_mask(self, sz):
-                mask = (torch.triu(torch.ones(sz, sz)) == 1).transpose(0, 1)
-                mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, float(0.0))
-                return mask
-
-            def forward(self, src, srcmask):
-                src = self.encoder(src)
-                src = self.pos_encoder(src)
-                output = self.transformer_encoder(src.transpose(0,1), srcmask).transpose(0,1)
-                output = self.linear(output)[:,:,0]
-                output = self.linear2(output)
-                return output
-
-        class PositionalEncoding(nn.Module):
-            def __init__(self, d_model, dropout=0.1, max_len=5000):
-                super(PositionalEncoding, self).__init__()
-                self.dropout = nn.Dropout(p=dropout)
-
-                pe = torch.zeros(max_len, d_model)
-                position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
-                div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
-                pe[:, 0::2] = torch.sin(position * div_term)
-                pe[:, 1::2] = torch.cos(position * div_term)
-                pe = pe.unsqueeze(0).transpose(0, 1)
-                self.register_buffer('pe', pe)
-
-            def forward(self, x):
-                x = x + self.pe[:x.size(0), :]
-                return self.dropout(x)
-
-        def gen_attention_mask(x):
-            mask = torch.eq(x, 0)
-            return mask
-
-
-        device = torch.device("cuda")
-
-        lr = 1e-4
-        model = TFModel(30*2, 30, 512, 8, 4, 0.1).to(device) # 7*2, 7,
-        criterion = nn.MSELoss()
-        optimizer = torch.optim.Adam(model.parameters(), lr=lr)
-
-
-        from tqdm import tqdm
-        epoch = 1000
-        model.train()
-        progress = tqdm(range(epoch))
-        for i in progress:
-            batchloss = 0.0
-            for (inputs, outputs) in train_loader:
-                optimizer.zero_grad()
-                src_mask = model.generate_square_subsequent_mask(inputs.shape[1]).to(device)
-                result = model(inputs.float().to(device),  src_mask)
-                loss = criterion(result, outputs[:,:,0].float().to(device))
-                loss.backward()
-                optimizer.step()
-                batchloss += loss
-            progress.set_description("loss: {:0.6f}".format(batchloss.cpu().item() / len(train_loader)))
-
-
-        def evaluate():
-            input = torch.tensor(train_data[-30*2:]).reshape(1,-1,1).to(device).float().to(device) # -7*2
-            model.eval()
-            
-            src_mask = model.generate_square_subsequent_mask(input.shape[1]).to(device)
-            predictions = model(input, src_mask)
-            return predictions.detach().cpu().numpy()
-
-
-        result = evaluate()
-        result = scaler.inverse_transform(result)[0]
-        real = data["종가"].to_numpy()
-        real = scaler.inverse_transform(real.reshape(-1,1))[:,0]
-    
-        ma_data = pd.read_csv('./{}/{}'.format(media, media), encoding='cp949')
-        ma30 = ma_data['종가'].rolling(window=100).mean()
-        ma_data.insert(len(ma_data.columns), "MA100", ma30)
-        ma_data_30 = []
-        for i in range(0, 30):
-            a = 0.3 * ma_data['MA30'].iloc[119+i] + 0.7 * result[i]
-            ma_data_30.append(a)
-
-        ma_data_30.to_csv(f"{media_dir}/predict.csv", encoding='cp949')
+    total = pd.DataFrame(total).transpose()
+    #total = total.astype(int)
+    total.to_csv(f"{data_dir}/predict.csv", encoding='cp949', header=False, index=False)
 
 if __name__=="__main__":
     update_data()
--- webapp/stock/views.py
+++ webapp/stock/views.py
@@ -3,12 +3,9 @@ from main.models import Main
 import numpy as np
 import pandas as pd
 import os, glob
-from sklearn.ensemble import RandomForestRegressor
 from pykrx import stock as pystock
 from datetime import datetime, timedelta
 from pytz import timezone
-from .data_update import update_data
-from sklearn.preprocessing import StandardScaler
 import random
 
 def home(request):
@@ -79,12 +76,9 @@ def stock(request):
                                                 'l5price':lowfive[4][2]})
 
 def stock_detail_dj(request):
-    
-
     if request.method == 'POST':
         word = request.POST.get('stockname').split('-')[-1]
-        # print(word)
-        predicted_stock_price = 0
+
         quantity = request.POST.get('quantity')
         # print(quantity)
         df = pd.read_csv('webapp/media/{}/{}.csv'.format(word, word), encoding='cp949')
@@ -94,90 +88,19 @@ def stock_detail_dj(request):
         predictprice = predictprice[word]
         # print(predictprice)
         predictprice = predictprice.iloc[int(quantity)]
+        
+        past_real = df["종가"][-7:]
+        #future = pd.read_csv('webapp/media/predict.csv').iloc[:int(quantity)]
+        df3 = pd.DataFrame(range(0, 20, 1))
+        df4 = pd.concat([df3, past_real], axis=1)
+        df = past_real.values.tolist()
+
         # print(predictprice)
         surgerate = round(100*((predictprice-currentprice)/currentprice),2)
         if surgerate>0:
             triangle = 1
         else:
             triangle = None
-        # xy = df.iloc[:,1:5].values
-        # window, past, predict_day, future_price = 7, 8, 1, 5       
-        # trainSet, testSet = MinMaxScaler(xy[0:-past]), MinMaxScaler(xy[-past-window:])  # past 가장 최근의 과거 일주일만 보여줌
-
-        # def buildDataSet(data, window):
-        #     xdata = []
-        #     ydata = []
-        #     for i in range(0, len(data) - window - predict_day):
-        #         xdata.append(data[i:i + window])                           # 행은 10개씩, 열은 직전의 Number 값들도 같이 입력변수로 넣어준다
-        #         ydata.append(data[i + window + predict_day - 1,:])                   # 행은 그 다음 행 하나랑, 열은 모든 feature
-        #     return np.array(xdata), np.array(ydata)
-
-        # X_train, y_train=buildDataSet(trainSet, window)
-        # X_test, y_test=buildDataSet(testSet, window)
-
-        # tmp_y_train, tmp_y_test = y_train[:,-1], y_test[:,-1]   # 예측할 때는 맨 마지막 종가 예측 결과만 출력해준다
-
-        # # RF는 2차원 데이터를 입력으로 받아서 1차원으로 바꿔줘야한다.
-        # X_train = X_train.reshape(X_train.shape[0],7*4)
-        # X_test = X_test.reshape(X_test.shape[0],7*4)
-        # tmp_y_train = tmp_y_train.reshape(X_train.shape[0],)
-        # tmp_y_test = tmp_y_test.reshape(X_test.shape[0],)
-
-        # model = RandomForestRegressor()
-        # model.fit(X_train,y_train)
-
-        # predicted_stock_price = model.predict(X_test)[:,-1]  # 예측할 때는 맨 마지막 종가 예측 결과만 출력해준다
-        # #print(df.iloc[-past:,-1])
-        # #print("가자아아아ㅏ앙", df.iloc[-past:,-1])
-        # predicted_stock_price = back_MinMax(df.iloc[-past:,-1], predicted_stock_price)
-        
-        # #print("예측된 결과는 : ", predicted_stock_price.shape)
-        # #print("원본 테스트 크기 :", tmp_y_test.shape)
-        # #print("이;겆",xy.shape, model.predict(X_test)[-1].reshape(1,4).shape)
-        # xy = np.append(xy, model.predict(X_test)[-1].reshape(1,4), axis=0)   # 예측된 맨 마지막 결과를 넣어서 이제부터 미래 예측
-        # for i in range(future_price):
-        #     trainSet, testSet = MinMaxScaler(xy[0:-past]), MinMaxScaler(xy[-past-window:])  # past 가장 최근의 과거 일주일만 가지고 새로 학습
-
-        #     X_train, y_train = buildDataSet(trainSet, window)
-        #     X_test, y_test = buildDataSet(testSet, window)
-        #     #print("테스트는?",X_test.shape )
-        #     # RF는 2차원 데이터를 입력으로 받아서 1차원으로 바꿔줘야한다.
-        #     X_train = X_train.reshape(X_train.shape[0],7*4)
-        #     X_test = X_test.reshape(X_test.shape[0],7*4)
-        #     y_train = y_train.reshape(y_train.shape[0],4)
-
-        #     #model.fit(X_train, y_train)
-
-        #     future_predicted_stock_price = model.predict(X_test)
-        #     #print("이거", df.iloc[-past:,])
-        #     future_predicted_stock_price = back_MinMax(df.iloc[-past:,-1], future_predicted_stock_price)  # 여기서는 전체로 되돌려줌
-
-        #     #print("응", future_predicted_stock_price.shape)
-        #     xy = np.append(xy, future_predicted_stock_price[-1].reshape(1,4), axis=0)   # 예측된 맨 마지막 결과를 넣어서 이제부터 미래 예측
-
-        # org_df1 = pd.DataFrame(back_MinMax(df.iloc[-5:,-1], tmp_y_test)).reset_index(drop=True)
-        # old = pd.DataFrame(predicted_stock_price)
-        # new = pd.DataFrame(xy[-future_price:, -1])
-        # df1 = pd.concat([org_df1, new], axis=0).reset_index(drop=True)
-        # df2 = pd.concat([old, new], axis=0).reset_index(drop=True)
-        # df3 = pd.DataFrame(range(0, predicted_stock_price.shape[0] + future_price, 1)).reset_index(drop=True)
-        # df4 = pd.concat([df3, df1, df2], axis=1)
-        # # df4 = df4.fillna(0)
-        # df = df4.values.tolist()
-        # #print("df가 어찌되는겨", df4)
-        # #print("더해진 5개가 있나요?", xy.shape)
-        # ratio = -1
-        
-        # it = 0
-        # dt = 0
-        # n = len(predicted_stock_price)
-        # for i in range(n):
-        #     for j in range(i + 1, n):
-        #         if ((predicted_stock_price[j] / predicted_stock_price[i]) > ratio):
-        #             ratio = predicted_stock_price[j] / predicted_stock_price[i]
-        #             it = i
-        #             dt = j
-        # ratio=int(ratio*10000)
         if surgerate>20:
             opinion= 4
         elif surgerate>10:
